1ed90f08afb3dc5d09a1c8980d2ce91d9d4107a6
Haseeb <haseeb_heaven@yahoo.com>
2024-01-08 07:09:21 +0530

    Update version to 1.8.4

--- open_code_interpreter/libs/interpreter_lib.py
+++ open_code_interpreter/libs/interpreter_lib.py
@@ -28,7 +28,7 @@ from dotenv import load_dotenv
 class Interpreter:
     logger = None
     client = None
-    interpreter_version = "1.8.3"
+    interpreter_version = "1.8.4"
     
     def __init__(self, args):
         self.args = args
